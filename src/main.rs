0f546a74eb948faaff003b50bfef6b4d5b0a14e1
Pierre Baonla <pierrelandrybb@gmail.com>
2021-12-01 20:44:33 +0100

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+use advent_of_code::{Config, run};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    let increases_count = run(config).unwrap_or_else(|err| {
+        println!("Application error: {}", err);
+        process::exit(1);
+    });
+
+    println!("The number of increases is {:?}", increases_count);
 }
+
