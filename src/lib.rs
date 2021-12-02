8b4c895a9e5dbca73ff1cbb45f06bd8ddf7e8f57
p.baonla.bassom <pbaonla@octo.com>
2021-12-02 15:32:50 +0100

    day-2: first part

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,36 @@ pub fn run(config: Config) -> Result<i32, Box<dyn Error>> {
     let files_contents = fs::read_to_string(config.filename)?;
 
     let mut str_vec: Vec<&str> = files_contents.split("\n").collect();
-    // remove the last line
+
     str_vec.pop().unwrap();
 
-    // convert Vec<&str> to Vec<&i32>
-    let mut int_vecs = Vec::new();
+    let mut tuple_vecs: Vec<(&str, i32)> = Vec::new();
+
     for v in str_vec {
-        int_vecs.push(v.parse::<i32>().unwrap());
+        let mut split = v.split(" ");
+        let my_tuple = (split.next().unwrap(), split.next().unwrap().parse::<i32>().unwrap());
+        tuple_vecs.push(my_tuple);
     }
 
-    // Count the number of increases
-    let mut measurement_inc = 0;
-    for i in 0..int_vecs.len() - 3 {
-        let right = int_vecs[i + 1] + int_vecs[i + 2] + int_vecs[i + 3];
-        let left = int_vecs[i] + int_vecs[i + 1] + int_vecs[i + 2];
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
 
-        if (right - left) > 0 {
-            measurement_inc = measurement_inc + 1;
+    for tuple in tuple_vecs {
+        if tuple.0.eq("forward") {
+            x = x + tuple.1;
+        }
+
+        if tuple.0.eq("down") {
+            y = y + tuple.1;
+        }
+
+        if tuple.0.eq("up") {
+            y = y - tuple.1;
         }
     }
 
-    Ok(measurement_inc)
+    let product = x * y;
+    Ok(product)
 }
 
 pub struct Config {
